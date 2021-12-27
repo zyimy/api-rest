70530bfa845be8a766531c64e18ce6e166854e31
Yimy Zepeda <zepedayimy45@gmail.com>
2021-12-27 12:27:02 +0100

    origin

--- src/main/java/com/probando/apiService/controller/ApiController.java
+++ src/main/java/com/probando/apiService/controller/ApiController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.probando.apiService.ApiRepository;
 import com.probando.apiService.modelo.Tarea;
-
+@CrossOrigin(origins = "https://apiservicesspri.herokuapp.com",maxAge  =3600)
 @RestController
 @RequestMapping("/tareas")
 public class ApiController {
